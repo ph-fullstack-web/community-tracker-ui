d176b64bb8296b966fc167bdd4fb78777672ac92
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-05-23 21:48:07 +0800

    Created Mock data

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://localhost:8000'
+    // change when API ready to integrate
+    baseURL: 'http://localhost:3000'
 })
