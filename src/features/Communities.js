f4255d9c7afb97be5c23ee171eb91966c9a6e7ca
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-05-24 22:11:39 +0800

    Updated communities to use sub components

--- src/features/Communities.js
+++ src/features/Communities.js
@@ -1,26 +1,17 @@
-
-import { useEffect } from "react";
-import useGetCommunities from "hooks/useGetCommunities";
 import Logo from "components/Layout/Logo";
 import PageTitle from "components/Layout/PageTitle";
 import PageContainer from "components/Layout/PageContainer";
 import CommunityList from "components/community/CommunityList";
 
-
 const Communities = () => {
-    const {loading, data: communityData} = useGetCommunities();
-    useEffect(() => {
-    }, [loading, communityData])
-    
-    return (
-        <PageContainer>
-            <Logo/>
-            <PageTitle title="Community selection page." />
-            
-            <CommunityList loading={loading} communities={communityData} />
+  return (
+    <PageContainer>
+      <Logo />
+      <PageTitle title="Community selection page." />
 
-        </PageContainer>
-    )
-}
+      <CommunityList />
+    </PageContainer>
+  );
+};
 
 export default Communities;
