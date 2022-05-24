b15cd8439c4132f417423eeb28cc23bbc2030555
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-05-24 15:45:53 +0800

    Add react-query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Communities from "features/Communities";
 
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/communities" element={<Communities/>} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/communities" element={<Communities/>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
