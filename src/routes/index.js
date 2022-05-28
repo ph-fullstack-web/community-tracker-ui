47bdb41d4cd6e6fcfbbf63d9081c49c6805e6aac
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-05-28 08:42:32 +0800

    update code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 import { lazy } from "react";
 
+
+/* TODO:
+    1. Please follow lazy import instead of the traditional way of importing the components.
+*/
 const Communities = lazy(() => import("../features/communities"));
+
 const routes = [
   {
     path: "/communities",
