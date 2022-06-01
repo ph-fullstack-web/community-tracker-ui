2d2a5c09ee58641c4d70891dce56bced679b5a34
nicsondelatorre <nicsondelatorre+skulp@gmail.com>
2022-06-01 10:35:19 +0800

    add auto complete and insertion of new records

--- src/common/InputChip/index.js
+++ src/common/InputChip/index.js
@@ -1,79 +1,45 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import React, { useState } from 'react';
 import Chip from '@mui/material/Chip';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import TagFacesIcon from '@mui/icons-material/TagFaces';
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
-/* for typescript only
-interface ChipData {
-  key: number;
-  label: string;
-}
-*/
-
-const ListItem = styled('li')(({ theme }) => ({
-  margin: theme.spacing(0.5),
-}));
-
-export default function ChipsArray() {
-  const [chipData, setChipData] = React.useState([
-    { key: 0, label: 'Angular' },
-    { key: 1, label: 'jQuery' },
-    { key: 2, label: 'Polymer' },
-    { key: 3, label: 'React' },
-    { key: 4, label: 'Vue.js' },
-  ]);
-
-  const handleDelete = (chipToDelete) => () => {
-    setChipData((chips) =>
-      chips.filter((chip) => chip.key !== chipToDelete.key)
-    );
-  };
+export default function AutocompleteInputChip(props) {
+  const { options, setOptions } = props;
+  const [value, setValue] = useState([]);
 
   const handlePressEnter = (value) => {
-    setChipData([...chipData, { key: chipData.length, label: value }]);
+    setOptions([...options, { key: options.length, label: value }]);
   };
 
   return (
-    <Box noValidate autoComplete="on">
-      <Paper
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexWrap: 'wrap',
-          listStyle: 'none',
-          p: 0.5,
-          m: 0,
-        }}
-        component="ul"
-      >
-        {chipData.map((data) => {
-          let icon;
-
-          return (
-            <ListItem key={data.key}>
-              <Chip
-                icon={icon}
-                label={data.label}
-                onDelete={handleDelete(data)}
-              />
-            </ListItem>
-          );
-        })}
-      </Paper>
-      <TextField
-        id="chip-text-input"
-        label="Skills"
-        variant="standard"
-        onKeyPress={(e) => {
-          if (e.key === 'Enter') {
-            handlePressEnter(e.target.value);
-            e.target.value = '';
-          }
-        }}
-      />
-    </Box>
+    <Autocomplete
+      multiple
+      id="auto-complete-chip"
+      value={value}
+      onChange={(event, newValue) => {
+        setValue([...newValue.filter((option) => option)]);
+      }}
+      options={options}
+      getOptionLabel={(option) => option.label}
+      renderTags={(tagValue, getTagProps) =>
+        tagValue.map((option, index) => (
+          <Chip label={option.label} {...getTagProps({ index })} />
+        ))
+      }
+      style={{ width: '100%' }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Skills"
+          placeholder="Skills"
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handlePressEnter(e.target.value);
+              //e.target.value = '';
+            }
+          }}
+        />
+      )}
+    />
   );
 }
