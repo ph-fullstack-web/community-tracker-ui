b806823c664baa5c075b8f6aa7c518399e60c4ec
John Lawrence Co <johnlawrence.co@ph.softvision.global>
2022-06-02 16:11:45 +0800

    add minimum height to PageContainer

--- src/layout/PageContainer.js
+++ src/layout/PageContainer.js
@@ -1,17 +1,19 @@
-import { Container } from "@mui/material"
+import { Container } from '@mui/material';
 
-const PageContainer = ({children}) => {
-    return (
-        <Container maxWidth='xl' sx={{ 
-            borderRadius: '15px',
-            backgroundColor: "#F3F6F8",
-            padding: '1px',
-            marginTop: '2rem'
-            }} 
-        >
-            {children}
-        </Container>
-    )
-}
+const PageContainer = ({ children }) => {
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        borderRadius: '15px',
+        backgroundColor: '#F3F6F8',
+        padding: '1px',
+        marginTop: '2rem',
+        minHeight: '95vh',
+      }}>
+      {children}
+    </Container>
+  );
+};
 
 export default PageContainer;
