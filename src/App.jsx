f0bffc58dc973a85ce685d3a2fa531b1497f8b2b
John Lawrence Co <johnlawrence.co@softvision.com>
2022-06-03 15:21:04 +0800

    create color theme and add ThemeProvider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import routeList from "routes";
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import routeList from 'routes';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+
+import theme from 'theme';
 
 function App() {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          {routeList.map(({ element: Element, name, path }) => (
-            <Route key={name} path={path} element={Element} />
-          ))}
-        </Routes>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            {routeList.map(({ element: Element, name, path }) => (
+              <Route key={name} path={path} element={Element} />
+            ))}
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import createTheme from '@mui/material/styles/createTheme';
+
+const theme = createTheme({
+  palette: {
+    plum: {
+      light: '#85A0F9',
+      medium: '#7373D8',
+      dark: '#2E308E',
+    },
+    blue: {
+      light: '#92BBE6',
+      medium: '#6AA2DC',
+      dark: '#2F78C4',
+    },
+    teal: {
+      light: '#97F5F7',
+      medium: '#26EFE9',
+      dark: '#06C7CC',
+    },
+  },
+});
+
+export default theme;
