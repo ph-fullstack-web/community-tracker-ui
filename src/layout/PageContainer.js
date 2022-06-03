5e059248ab640a32e9b491a84e802866f9feb9d6
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-06-03 13:55:55 +0800

    FEATURE/CT-134

--- src/layout/PageContainer.js
+++ src/layout/PageContainer.js
@@ -1,17 +1,21 @@
-import { Container } from "@mui/material"
+import { Container } from "@mui/material";
+import Navbar from "./Navbar";
 
-const PageContainer = ({children}) => {
-    return (
-        <Container maxWidth='xl' sx={{ 
-            borderRadius: '15px',
-            backgroundColor: "#F3F6F8",
-            padding: '1px',
-            marginTop: '2rem'
-            }} 
-        >
-            {children}
-        </Container>
-    )
-}
+const PageContainer = ({ children }) => {
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        borderRadius: "15px",
+        backgroundColor: "#F3F6F8",
+        padding: "1px",
+        marginTop: "2rem",
+      }}
+    >
+      <Navbar />
+      {children}
+    </Container>
+  );
+};
 
 export default PageContainer;
