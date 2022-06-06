b40c0f1e437f41b061fa707382f067ab36a2edb2
John Lawrence Co <johnlawrence.co@softvision.com>
2022-06-06 21:46:58 +0800

    update PageContainer to include light-colored border when in dark mode

--- src/layout/PageContainer.jsx
+++ src/layout/PageContainer.jsx
@@ -3,13 +3,18 @@ import { Container } from '@mui/material';
 import { SwitchThemeContext } from 'contexts/Theme/SwitchThemeContext';
 
 const PageContainer = ({ children }) => {
-  const { currentTheme } = useContext(SwitchThemeContext);
+  const { currentTheme, currentThemePalette } = useContext(SwitchThemeContext);
   return (
     <Container
       maxWidth="xl"
       sx={{
         borderRadius: '15px',
-        backgroundColor: currentTheme === 'dark' ? 'dark.dark' : '#F3F6F8',
+        backgroundColor:
+          currentTheme === 'dark' ? currentThemePalette.dark : '#F3F6F8',
+        border:
+          currentTheme === 'dark'
+            ? `1px solid ${currentThemePalette.light}`
+            : null,
         padding: '1px',
         marginTop: '2rem',
       }}>
