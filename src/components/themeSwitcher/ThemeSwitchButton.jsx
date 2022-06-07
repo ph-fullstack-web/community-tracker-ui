22cd25c46228a127019d8d71e2bb84c72d921caf
John Lawrence Co <johnlawrence.co@softvision.com>
2022-06-07 15:25:46 +0800

    remove unneeded currentTheme random renders which were used for tracking theme

--- src/components/themeSwitcher/ThemeSwitchButton.jsx
+++ src/components/themeSwitcher/ThemeSwitchButton.jsx
@@ -39,7 +39,6 @@ const ThemeSwitchButton = () => {
 
   return (
     <>
-      <div>{currentTheme}</div>
       <IconButton
         variant="outlined"
         onClick={handleToggle}
--- src/features/communities/Communities.jsx
+++ src/features/communities/Communities.jsx
@@ -3,19 +3,10 @@ import PageTitle from 'layout/PageTitle';
 import PageContainer from 'layout/PageContainer';
 import CommunityList from 'components/community/CommunityList';
 
-import { Typography } from '@mui/material';
-import useSwitchThemeContext from 'hooks/Theme/useSwitchThemeContext';
-
 const Communities = () => {
-  const { currentTheme } = useSwitchThemeContext();
-
   return (
     <PageContainer>
       <Logo />
-
-      <Typography variant="h5" gutterBottom component="div">
-        {currentTheme}
-      </Typography>
 
       <PageTitle title="Community selection page." />
 
