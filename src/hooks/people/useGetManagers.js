affd436cf83cce2aba55b96a816626084be4f5c9
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-06-09 23:26:28 +0800

    update

--- src/hooks/people/useGetManagers.js
+++ src/hooks/people/useGetManagers.js
@@ -1,21 +1,22 @@
 import { useQuery } from "react-query";
 import { getManagers } from "services/people-service/GetManagers";
 
-
 const useGetManagers = () => {
-    
-    const {isLoading, isError, data, error, refetch} = useQuery('managers', () => getManagers(), {
-        staleTime: 10000
-    });
-
-    return {
-        isLoading,
-        isError,
-        error,
-        data,
-        refetch
+  const { isLoading, isError, data, error, refetch } = useQuery(
+    "managers",
+    () => getManagers(),
+    {
+      staleTime: 10000,
     }
+  );
 
-}
+  return {
+    isLoading,
+    isError,
+    error,
+    data,
+    refetch,
+  };
+};
 
 export default useGetManagers;
