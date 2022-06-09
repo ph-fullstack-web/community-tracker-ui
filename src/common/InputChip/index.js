5eb74b7a24e14dc960350f07d3e78489d1155953
nicsondelatorre <nicsondelatorre+skulp@gmail.com>
2022-06-09 23:27:41 +0800

    [CT-135] - add checking of duplicate records

--- src/common/InputChip/index.js
+++ src/common/InputChip/index.js
@@ -1,37 +1,44 @@
-import React, { useState } from 'react';
-import Chip from '@mui/material/Chip';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
 export default function AutocompleteInputChip(props) {
-  const { options, setOptions } = props;
-  const [value, setValue] = useState([]);
+  const { options, setOptions, setSelectedValue, setNewValues, newValues } =
+    props;
+  //const [value, setValue] = useState(options[0]);
+
+  const getMaxId = () => {
+    return Math.max(...options.map((opt) => opt.id)) + 1;
+  };
 
   const handlePressEnter = (value) => {
-    setOptions([...options, { key: options.length, label: value }]);
+    const labels = options.map((opt) => opt.label);
+    const included = labels.includes(value);
+    const newId = getMaxId();
+    if (!included) {
+      setOptions([...options, { id: newId, label: value }]);
+      setNewValues([...newValues, { id: newId, label: value }]);
+    }
   };
 
   return (
     <Autocomplete
       multiple
       id="auto-complete-chip"
-      value={value}
+      //value={value}
       onChange={(event, newValue) => {
-        setValue([...newValue.filter((option) => option)]);
+        setSelectedValue([...newValue.filter((option) => option)]);
       }}
       options={options}
       getOptionLabel={(option) => option.label}
-      renderTags={(tagValue, getTagProps) =>
-        tagValue.map((option, index) => (
-          <Chip label={option.label} {...getTagProps({ index })} />
-        ))
-      }
+      filterSelectedOptions
       sx={{ width: '100%' }}
       renderInput={(params) => (
         <TextField
           {...params}
           label="Skills"
           placeholder="Skills"
+          key={params.id}
           onKeyPress={(e) => {
             if (e.key === 'Enter') {
               handlePressEnter(e.target.value);
