c9492279f2b75e814dfab4ce7f3b29bce1455566
Arvin De Los Santos <arvin.delossantos@softvision.com>
2022-06-10 21:11:51 +0800

    redirect to communities on save

--- src/features/communities/AddCommunity.jsx
+++ src/features/communities/AddCommunity.jsx
@@ -2,13 +2,19 @@ import PageTitle from "layout/PageTitle";
 import PageContainer from "layout/PageContainer";
 import CommunityForm from "components/community/CommunityForm";
 import { addCommunityService } from "services";
+import { useNavigate } from "react-router-dom";
 
 
 const AddCommunity = () => {
+  const navigator = useNavigate()
+  const onSaveCommunity = async (data) => {
+    await addCommunityService(data);
+    navigator('/communities')
+  }
   return (
     <PageContainer>
       <PageTitle title="Community Input Page." />
-      <CommunityForm onClickHandler={addCommunityService} buttonText={"SAVE"} />
+      <CommunityForm onClickHandler={onSaveCommunity} buttonText={"SAVE"} />
     </PageContainer>
   );
 };
