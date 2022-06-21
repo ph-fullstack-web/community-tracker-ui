9fcc3c3900b60fa4c703f28bad5f7ac532e1fb53
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-06-21 13:56:56 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import routeList from 'routes';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { SwitchThemeContextProvider } from 'contexts/theme/SwitchThemeContext';
 import { AuthProvider } from 'contexts/auth/AuthContext';
 import { NotificationProvider } from 'contexts/notification/NotificationContext';
+import routes from 'routes';
 
 
 function App() {
@@ -13,16 +13,16 @@ function App() {
     <SwitchThemeContextProvider>
       <AuthProvider>
         <NotificationProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Routes>
-              {routeList.map(({ element: Element, name, path }) => (
-                <Route key={name} path={path} element={Element} />
-              ))}
-            </Routes>
-          </BrowserRouter>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+          <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <Routes>
+                {routes.map(({ element: Element, name, path }) => (
+                  <Route key={name} path={path} element={Element} />
+                ))}
+              </Routes>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </NotificationProvider>
       </AuthProvider>
     </SwitchThemeContextProvider>
