54de25334a411a943203b980045abbb7ac4949f4
John Lawrence Co <johnlawrence.co@softvision.com>
2022-07-01 08:06:27 +0800

    apply and adjust themes to AutoCompleteChipInput (not complete)

--- src/components/common/AutoCompleteInputChip/index.js
+++ src/components/common/AutoCompleteInputChip/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import FormTextField from '../form-controls/FormTextField';
+import { useSwitchThemeContext } from 'hooks';
 
 export default function AutocompleteInputChip(props) {
+  const { currentTheme, currentThemePalette }  = useSwitchThemeContext();
+  const themeForDarkOnly = (color) => (currentTheme === "dark" ? color : null);
+
   const { options, setOptions, setSelectedValue, setNewValues, newValues } =
     props;
   //const [value, setValue] = useState(options[0]);
@@ -32,9 +36,34 @@ export default function AutocompleteInputChip(props) {
       options={options}
       getOptionLabel={(option) => option.label}
       filterSelectedOptions
-      sx={{ width: '100%' }}
+      sx={{ 
+        width: '100%',
+        "& .MuiChip-root": {
+          border: `1px solid ${themeForDarkOnly(currentThemePalette.light)}`,
+          backgroundColor: themeForDarkOnly("#3C4043"),
+          color: themeForDarkOnly("#FFFFFF")
+        },
+        "& .MuiChip-deleteIcon": {
+          color: themeForDarkOnly(currentThemePalette.light)
+        },
+        "& .MuiSvgIcon-root": {
+          color: themeForDarkOnly(currentThemePalette.light),
+        },
+        // (Targets the dropdown part but does not work):
+        // "& .MuiAutocomplete-paper": {
+        //   borderRadius: themeForDarkOnly(1),
+        //   border: themeForDarkOnly(
+        //     `1px solid ${currentThemePalette.light}`
+        //   ),
+        //   backgroundColor: currentThemePalette.bgPrimary,
+        //   color: currentThemePalette.text,
+        //   "& .MuiMenuItem-root:hover": {
+        //     backgroundColor: themeForDarkOnly("#293A46"),
+        //   },
+        // },
+      }}
       renderInput={(params) => (
-        <TextField
+        <FormTextField
           {...params}
           label="Skills"
           placeholder="Skills"
