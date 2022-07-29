0927d55e5e5faf346f4948d1088b1a85e6e9cf32
Baron Paredes <baronpatrick.paredes@gmail.com>
2022-07-30 00:18:17 +0800

    fix(casing) - fix import casing

--- src/components/index.js
+++ src/components/index.js
@@ -11,7 +11,7 @@ import FormSwitch from "./common/form-controls/FormSwitch";
 import NoDataTable from "./common/NoDataTable";
 import * as Maintenance from "./maintenance";
 import ConfirmModal from "./common/ConfirmModal";
-import PieChartDashBoard from "./Dashboards/SinglePieChart"
+import PieChartDashBoard from "./dashboards/SinglePieChart"
 import SkillsCard from "./skills/SkillsCard"
 
 export {
